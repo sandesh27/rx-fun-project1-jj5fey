44a32df7b5bea3412d602a490abb4299945c9d07
sandesh27 <sandesh.s_magdum@yahoo.co.in>
2021-05-15 20:06:28 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import { fromEvent, combineLatest } from 'rxjs';
-import { map, startWith, tap } from 'rxjs/operators';
+import { fromEvent, interval, animationFrameScheduler } from 'rxjs';
+import { map, startWith, tap, observeOn } from 'rxjs/operators';
 
 console.clear();
 
-const rangeEl: HTMLInputElement = document.querySelector('input[name=range]');
+const rangeEl: HTMLInputElement = document.querySelector('#range');
 const rangeValueEl: HTMLInputElement = document.querySelector('.range-value');
 const doubledValueEl: HTMLInputElement = document.querySelector(
   '.doubled-value'
 );
+
+const intervals = interval(10);
 
 const range$ = fromEvent(rangeEl, 'input').pipe(
   map(e => e.target.value),
@@ -21,3 +23,11 @@ const double$ = range$.pipe(
 );
 
 const doubleSubcription = double$.subscribe();
+
+intervals
+  .pipe(
+    observeOn(animationFrameScheduler) // ...but we will observe on animationFrame
+  ) // scheduler to ensure smooth animation.
+  .subscribe(val => {
+    rangeEl.value = val + 1;
+  });
